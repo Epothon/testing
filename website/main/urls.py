388aec45003f12d344265cd6d41c377be18132a9
Epothon <mot842@gmail.com>
2024-01-30 02:57:19 +0100

    lots of changes

---
+++ website/main/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+from django.shortcuts import render
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
+
+ 
--- website/website/settings.py
+++ website/website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +76,16 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import os
+import dj_database_url
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 
 # Password validation
